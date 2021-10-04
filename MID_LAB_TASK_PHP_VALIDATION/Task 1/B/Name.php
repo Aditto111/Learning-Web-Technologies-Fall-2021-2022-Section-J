c684107bf1a2a53f6391c544c6a6c6cac8679006
John Doe <johndoe@example.com>
2021-10-04 18:23:58 +0600

    php validation

--- MID_LAB_TASK_PHP_VALIDATION/Task 1/B/Name.php
+++ MID_LAB_TASK_PHP_VALIDATION/Task 1/B/Name.php
@@ -1,26 +1,24 @@
 <html>
 <head>
-    
+    <meta charset="utf-8">
     <title>Name Field </title>
 </head>
 <body>  
-    <form method="gets" action="#" >
+    <form method="POST" action="#" >
         <fieldset border-width: 3px>
             <legend>NAME</legend>
             
                         <input type="text" name="myname" value="<?php 
 
-    //print_r($_GET);
     
     if(isset($_REQUEST['submit']))
     {
         $str = $_REQUEST['myname'];
-        if($str >chr(2))
-            echo $str;
-         elseif($str <chr(2) )
-            echo "Not Fullfill";
-            else
-            echo "";
+       if (strlen($_POST["myname"])<2)
+            {
+              echo " $str-should be 2 or more";
+            }
+       
        
     }
     

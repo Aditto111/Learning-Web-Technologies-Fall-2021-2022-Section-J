21c62cbcbfc2893a857dd17138ea2b510680b33f
John Doe <johndoe@example.com>
2021-10-08 14:04:52 +0600

    basic login form

--- Login System/Signupcheck.php
+++ Login System/Signupcheck.php
@@ -1,19 +1,24 @@
 <?php
+   
+   session_start();
 
 	if(isset($_POST['submit'])){
 
 		if($_POST['username'] != ""){
 			if($_POST['password'] != ""){
 				if($_POST['email'] != ""){
-					header('location: login.html');
+				
+					$user = ['username'=> $_POST['username'], 'password'=> $_POST['password']];
+					$_SESSION['user'] = $user;
+					header('location: Loginform.html');
 				}else{
-					echo "Invalid email...";
+					echo "Invalid username";
+				}	
+				}else{
+					echo "Invalid password...";
 				}	
 			}else{
-				echo "Invalid password...";
+				echo "Invalid email...";
 			}
-		}else{
-			echo "Invalid username...";
-		}
-	}
+		
 ?>
